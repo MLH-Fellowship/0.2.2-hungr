1c8826a31d7561eb1404e8d5a9d32b572d2f539f
Anirudh Emmadi <kanna6501@gmail.com>
2020-06-05 00:17:02 -0500

    Working maps-api

--- maps/map.js
+++ maps/map.js
@@ -1,6 +1,8 @@
 // Library & config imports
 const axios = require("axios");
+const { DataStore } = require("@aws-amplify/datastore");
 const config = require("./config.json");
+const { Restaurant } = require("../src/models");
 
 // Config
 const PLACES_API_KEY = config.PLACES_API_KEY;
@@ -8,22 +10,45 @@ const LAT = config.LAT;
 const LONG = config.LONG;
 const CUISINE = config.CUISINE;
 
-const placeData = getPlacesWithCuisine();
-console.log(placeData);
+let url = "";
 
-function getPlacesWithCuisine() {
-  let promise = new Promise((res, rej) => {
+if (CUISINE == "None")
+  url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${LAT},${LONG}&radius=5000&type=restaurant&key=${PLACES_API_KEY}`;
+else
+  url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${LAT},${LONG}&radius=5000&type=restaurant&keyword=${CUISINE}&key=${PLACES_API_KEY}`;
+
+axios.get(url).then((response) => {
+  let length = response.data.results.length;
+  for (let i = 0; i < length; i++) {
+    let restaurantId = response.data.results[i].id;
+    let restaurantName = response.data.results[i].name;
+    let restaurantRating = response.data.results[i].rating;
+    let restaurantPhotoRef = response.data.results[i].photos[0].photo_reference;
+
     axios
       .get(
-        `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${LAT},${LONG}&radius=5000&type=restaurant&keyword=${CUISINE}&key=${PLACES_API_KEY}`
+        `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${restaurantPhotoRef}&key=${PLACES_API_KEY}`
       )
-      .then(function (response) {
-        res(response.data).then((data) => {
-          return data;
-        });
-      })
-      .catch(function (error) {
-        rej("Unable to retrieve restaurants...");
+      .then((res) => {
+        let restaurantPhoto = res.data;
+        data = {
+          id: restaurantId,
+          name: restaurantName,
+          rating: restaurantRating,
+          photo: restaurantPhoto,
+        };
+        pushData(data);
       });
-  });
+  }
+});
+
+async function pushData(data) {
+  await DataStore.save(
+    new Restaurant({
+      id: data.id,
+      name: data.name,
+      rating: data.rating,
+      photo: data.photo,
+    })
+  );
 }
